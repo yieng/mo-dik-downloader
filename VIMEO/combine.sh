817f6bd671318e9371bf10e772aa6ba9fe032513
Cassandra Lee <iamthegirlclaudia@yahoo.com>
2023-04-18 21:19:46 +0800

    Update combine.sh

--- VIMEO/combine.sh
+++ VIMEO/combine.sh
@@ -2,4 +2,4 @@
 # $2 = audio
 # $3 = output.mp4
 
-ffmpeg -i "/Users/lglab/Downloads/VIMEO/$1" -i "/Users/lglab/Downloads/VIMEO/$2" -c:v copy -c:a aac "/Users/lglab/Downloads/VIMEO/$3"
+ffmpeg -i "$PWD/$1" -i "$PWD/$2" -c:v copy -c:a aac "$PWD/$3"
