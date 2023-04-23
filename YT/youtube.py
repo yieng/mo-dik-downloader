1292144b31f541853fb65a007743e3406092ee62
Cassandra Lee <iamthegirlclaudia@yahoo.com>
2023-04-23 10:45:46 +0800

    Update youtube.py

--- YT/youtube.py
+++ YT/youtube.py
@@ -1,9 +1,13 @@
 from pytube import YouTube
 import pytube
+import re
 
 # make sure NO non-YouTube links are in the file.
 with open('youtube_urls.txt','r+') as f:
    YT = f.readlines()
+
+#g = open('youtube_title_des.txt','w+')
+#g.close()
 
 #YT = sorted(list(set(YT)))
 # print(YT)
@@ -15,8 +19,9 @@ for t in YT:
       print(t+"  ... TRY AGAIN"+t)
    else:
       yt = YouTube(t)
-      print(yt.title)
-      print('DOWNLOADING: '+ yt.title)
+      yt_title = yt.title
+      print(yt_title)
+      print('DOWNLOADING: '+ yt_title)
 
       #SUPER LOW RES - for plain music videos
       #yt.streams.first().download()
@@ -28,8 +33,17 @@ for t in YT:
       #yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
 
       # HI-RES + YouTube video ID
-      #yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(filename=t[17:-1])
+      #new_file_name = re.sub(r'[^\w]', '_', yt_title) + '_' + t.split('=')[1].replace('\n','')+'.mp4'
+      #yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').asc().first().download(filename=new_file_name)
 
-      print('DONE: '+ yt.title)
+      '''with open('youtube_title_des.txt','a+') as g:
+         g.write(yt_title)
+         g.write('\n')
+         g.write(str(yt.publish_date))
+         g.write('\n')
+         g.write(yt.description)
+         g.write('\n')'''
+         
+      print('DONE: '+ yt_title)
       print('====================================')
       #  A = input("press enter to continue...") 
